97adf8000377d522c3c481b25932323e8f4e18e5
Bratan <153600232+Olzhik06@users.noreply.github.com>
2025-02-18 14:21:38 +0500

    Add and Update

--- main.go
+++ main.go
@@ -1,22 +1,66 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/AskatNa/OnlineClinic/api/controllers"
 	"github.com/AskatNa/OnlineClinic/api/routes"
 	"github.com/AskatNa/OnlineClinic/config/configs"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	router := gin.Default()
-	client := configs.ConnectDB()
-	controllers.SetUserCollection(configs.GetCollection(client, "users"))
+var client *mongo.Client
 
-	router.LoadHTMLGlob("./ui/html/*")
-	routes.SetupRoute(router)
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var err error
+
+	// Подключение к MongoDB
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB:", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Couldn't connect to MongoDB:", err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+
+	// Инициализация коллекций
+	userCollection := client.Database("online_clinic").Collection("users")
+	walletCollection := client.Database("online_clinic").Collection("wallets")
+	transactionCollection := client.Database("online_clinic").Collection("transactions")
+
+	// Передаём коллекции в контроллер
+	controllers.SetCollections(userCollection, walletCollection, transactionCollection)
+
+	// Настройка роутера
+	router := gin.Default()
+	configs.ConnectDB()
+	router.LoadHTMLGlob("ui/html/*.html")
+
+	router.GET("/wallets", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "wallets.html", nil)
+	})
+
+	// Регистрация маршрутов
+	routes.UnauthRoutes(router)
 	routes.UserRoutes(router)
 	routes.DoctorRoutes(router)
+	routes.PatientRoutes(router)
+	routes.AppointmentRoutes(router)
+	routes.WalletRoutes(router) // Подключаем маршруты кошелька
+
 	fmt.Println("The server is running on port :9000")
-	router.Run(":" + configs.EnvPort())
+	log.Fatal(http.ListenAndServe(":9000", router))
 }
